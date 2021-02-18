b7be2254f2ef9df2b25d94eb57fd15c1f0195b45
hqian5 <haoqian721@163.com>
2021-02-18 16:17:31 +0800

    ProgressDialog的demo

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -1,36 +1,31 @@
 package com.example.demo1;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Context mContext;
-    private AlertDialog dialog;
-    private AlertDialog.Builder builder;
-    private Button btnStart;
-    private Button btnEnd;
-    private Button btnDialog1;
-    private Button btnDialog2;
-    private Button btnDialogDiy;
+    private ProgressDialog pd1;
+    private int progressStart = 0;
+    private final static int MAX_COUNT = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +36,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initView() {
-        btnStart = findViewById(R.id.btn_start);
-        btnEnd = findViewById(R.id.btn_end);
-        btnDialog1 = findViewById(R.id.btn_dialog3);
-        btnDialog2 = findViewById(R.id.btn_dialog4);
-        btnDialogDiy = findViewById(R.id.btn_dialog_diy);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnEnd = findViewById(R.id.btn_end);
+        Button btnDialog1 = findViewById(R.id.btn_dialog3);
+        Button btnDialog2 = findViewById(R.id.btn_dialog4);
+        Button btnDialogDiy = findViewById(R.id.btn_dialog_diy);
         btnStart.setOnClickListener(this);
         btnEnd.setOnClickListener(this);
         btnDialog1.setOnClickListener(this);
@@ -58,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         View toastView = getLayoutInflater().inflate(R.layout.item_child, findViewById(R.id.ll_child));
         ImageView imageView = toastView.findViewById(R.id.iv_child);
         TextView textView = toastView.findViewById(R.id.tv_child);
-        imageView.setImageResource(R.drawable.ic_alipay);
+        imageView.setImageResource(R.mipmap.ic_launcher_round);
         textView.setText(msg);
         Toast toast = new Toast(MainActivity.this);
         toast.setGravity(Gravity.CENTER, 0, 0);
@@ -73,134 +68,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start:
-                builder = new AlertDialog.Builder(mContext)
-                        .setIcon(R.drawable.ic_dva1)
-                        .setTitle("普通的dialog")
-                        .setMessage("这是一个普通的dialog，请选择以下三个选项")
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("你选择了确定");
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("你选择了取消");
-                            }
-                        })
-                        .setNeutralButton("不知道", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("你选择了不知道");
-                            }
-                        });
-                dialog = builder.create();
-                dialog.show();
+                //这里的话参数依次为 上下文, 标题, 内容, 是否显示进度, 是否可以点击其他区域关闭
+                ProgressDialog.show(mContext, "请稍等", "加载中...", false, true);
                 break;
             case R.id.btn_end:
-                String[] items = {"星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"};
-                builder = new AlertDialog.Builder(mContext)
-                        .setIcon(R.drawable.ic_dva2)
-                        .setTitle("请选择日期~")
-                        .setItems(items, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("你选择了" + items[which]);
-                            }
-                        });
-                dialog = builder.create();
-                dialog.show();
+                pd1 = new ProgressDialog(mContext);
+                pd1.setTitle("请稍后");
+                pd1.setMessage("加载中...");
+                pd1.setCancelable(true);
+                pd1.setIndeterminate(false);
+                //这里是设置进度条的风格,HORIZONTAL是水平进度条,SPINNER是圆形进度条
+                pd1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pd1.show();
                 break;
             case R.id.btn_dialog3:
-                String[] food = {"burger", "fish and chips", "steak", "fried chicken"};
-                builder = new AlertDialog.Builder(mContext)
-                        .setTitle("Please choose the main dish")
-                        .setIcon(R.drawable.ic_dva3)
-                        .setSingleChoiceItems(food, 0, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("You have chosen " + food[which]);
+                pd1 = new ProgressDialog(mContext);
+                pd1.setTitle("下载中");
+                pd1.setCancelable(true);
+                pd1.setIndeterminate(false);
+                pd1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                new Thread() {
+                    public void run() {
+                        for (int i = 0; i <= MAX_COUNT; i++) {
+                            progressStart = i;
+                            try {
+                                Thread.sleep(50);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
                             }
-                        })
-                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                setToast("Your dish will be delivered soon");
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        })
-                        .setCancelable(false);
-                dialog = builder.create();
-                dialog.show();
+                            handler.sendEmptyMessage(111);
+
+                        }
+                    }
+                }.start();
+                progressStart = 0;
                 break;
             case R.id.btn_dialog4:
-                String[] activities = {"football", "basketball", "card games", "cycling", "hiking", "running", "gym"};
-                boolean[] checkedItems = {false, false, false, false, false, false, false};
-                builder = new AlertDialog.Builder(mContext)
-                        .setTitle("Please choose activities you are going to do during your holiday")
-                        .setMultiChoiceItems(activities, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                                checkedItems[which] = isChecked;
-                            }
-                        })
-                        .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                StringBuilder stringBuilder = new StringBuilder("You have chosen");
-                                for (int i = 0; i < checkedItems.length; i++) {
-                                    if (checkedItems[i]) {
-                                        stringBuilder.append(" ").append(activities[i]);
-                                    }
-                                }
-                                setToast(stringBuilder.toString());
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-                dialog = builder.create();
-                dialog.show();
                 break;
             case R.id.btn_dialog_diy:
-                View dialogView = MainActivity.this.getLayoutInflater().inflate(R.layout.layout_dialog, null, false);
-                TextView tvTitle = dialogView.findViewById(R.id.tv_title);
-                TextView tvBody = dialogView.findViewById(R.id.tv_body);
-                Button btnConfirm = dialogView.findViewById(R.id.btn_confirm);
-                Button btnCancel = dialogView.findViewById(R.id.btn_cancel);
-                builder = new AlertDialog.Builder(mContext)
-                        .setView(dialogView)
-                        .setCancelable(false);
-                dialog = builder.create();
-                dialog.show();
-                tvTitle.setText("是否跳转");
-                tvBody.setText("访问百度一下");
-                btnConfirm.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        setToast("跳转至浏览器");
-                        Uri uri = Uri.parse("https://www.baidu.com");
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
-                        dialog.dismiss();
-                    }
-                });
-                btnCancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
                 break;
         }
     }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            if (msg.what == 111) {
+                pd1.setProgress(progressStart);
+                NumberFormat format = NumberFormat.getPercentInstance();
+                format.setMaximumIntegerDigits(3);
+                pd1.setMessage("已下载" + format.format(progressStart / 100.0));
+                pd1.show();
+            }
+            if (progressStart == MAX_COUNT) {
+                pd1.dismiss();
+                setToast("下载完成");
+            }
+            return true;
+        }
+    });
 }
